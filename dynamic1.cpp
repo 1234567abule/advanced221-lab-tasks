04b8a7128552278952d61244d2499f22be4866c6
1234567abule <bsc-45-21@unima.ac.mw>
2024-04-23 00:36:35 -0700

    adding comments

--- dynamic1.cpp
+++ dynamic1.cpp
@@ -2,16 +2,19 @@
 #include<string>
 using namespace std;
 int main(){
+    //user should enter an enteger
 cout<<"enter the value of the integer: ";
 int input;
 cin>>input;
+//int variable should be dynamically allocated memory and inialised
 int *bule = new int(input);
 
 cout<<"enter the value of the string : ";
 string anotherinput ;
 cin>>anotherinput;
+//string variable should be dynamically allocated memory and inialised
 string *blessings = new string(anotherinput);
-
+//displaying variables that were dynamicaly allocated memory
 cout<<"The value of the enteger is "<<*bule <<endl;
 cout<<"The value of the string is "<<*blessings <<endl;
 
