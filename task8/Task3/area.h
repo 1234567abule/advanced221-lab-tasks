249e58af3ca1ebe79622f1eb764fa70b1e9ed6a2
1234567abule <bsc-45-21@unima.ac.mw>
2024-05-10 18:27:56 -0700

    adding some practical staff

---
+++ task8/Task3/area.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "shapes.h"
+
+
+class Area {
+public:
+static float findSquareArea(shapes::Square square);
+static float findTriangleAreas(shapes::Triangle triangle);
+static float findCircleArea(shapes::Circle circle);
+
+};
+
---
+++ task8/Task3/shapes.h
@@ -0,0 +1,49 @@
+#pragma once
+
+
+namespace shapes
+{
+class Square{
+    private:
+    float sideLength;
+    public:
+    Square();
+    Square(float length);
+    ~Square();
+    float getsideLength();
+    void setsideLength(float length);
+};
+class Triangle
+{
+private:
+    float base;
+    float height;
+public:
+    Triangle();
+    Triangle(float basevalue,float heightvalue);
+    ~Triangle();
+    float getbase();
+    float getheight();
+    void setbase(float basevalue);
+    void setheight(float heightvalue);
+
+};
+class Circle
+{
+private:
+float radius;
+public:
+Circle();
+Circle(float radiusvalue);
+~Circle();
+float getradius();
+void setradius(float radiusvalue);
+
+};
+
+
+
+
+
+
+}
